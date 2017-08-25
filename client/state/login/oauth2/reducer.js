a9bc15b81f502d40592627df3d0b87590563c7ab
Ben Dwyer <ben@scruffian.com>
2017-08-25 13:57:21 +0100

    OAuth signup: This links the OAuth login form to the OAuth signup

--- client/state/login/oauth2/reducer.js
+++ client/state/login/oauth2/reducer.js
@@ -114,9 +114,7 @@ export const clients = createReducer( initialClientsData, {
 } );
 
 export const currentClientId = createReducer( null, {
-	[ ROUTE_SET ]: ( state, { path, query } ) => {
-		return ( startsWith( path, '/log-in' ) || startsWith( path, '/start' ) ) && query.client_id || null;
-	}
+	[ ROUTE_SET ]: ( state, { path, query } ) => startsWith( path, '/log-in' ) && query.client_id || null,
 } );
 
 export default combineReducers( {
